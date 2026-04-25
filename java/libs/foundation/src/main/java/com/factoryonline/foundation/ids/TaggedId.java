af3d176e2735b0d8860ed35e312df4ff47bf2e33
Lleyton Morris <lleyton92@gmail.com>
2026-04-25 17:39:02 +1000

    feat: refactor ClientId and SimulationId to extend TaggedId for improved ID management

---
+++ java/libs/foundation/src/main/java/com/factoryonline/foundation/ids/TaggedId.java
@@ -0,0 +1,48 @@
+package com.factoryonline.foundation.ids;
+
+import java.util.Objects;
+
+public abstract class TaggedId {
+    private final String value;
+
+    protected TaggedId(String value) {
+        this.value = validateNonBlank(value, "value");
+    }
+
+    public String value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+
+        TaggedId that = (TaggedId) other;
+        return value.equals(that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    private static String validateNonBlank(String value, String label) {
+        String validatedValue = Objects.requireNonNull(value, label);
+        if (validatedValue.isBlank()) {
+            throw new IllegalArgumentException(label + " must not be blank");
+        }
+
+        return validatedValue;
+    }
+}
